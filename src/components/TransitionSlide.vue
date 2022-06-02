cf89aa6874c3106f0aa5fa34e384a092e0c03b72
AlkarE <alkar.ew@gmail.com>
2022-06-02 15:43:08 +0300

    rename component

--- src/components/TransitionSlide.vue
+++ src/components/TransitionSlide.vue
@@ -1,29 +1,18 @@
 <template>
-  <transition-group
-    name="slide"
-    tag="ul"
-    appear
-    @enter="enter"
-    @after-enter="afterEnter"
-  >
+  <transition name="slide" appear @enter="enter" @after-enter="afterEnter">
     <slot />
-  </transition-group>
+  </transition>
 </template>
 
 <script>
 export default {
-  name: 'TransitiongSlide',
-  data() {
-    return {
-      index: 1
-    }
-  },
+  name: 'TransitionSlide',
+
   methods: {
     enter(el, done) {
-      el.dataset.index = this.index++
       el.style.opacity = 0
-      el.style.transform = 'translateY(-100%)'
-      const delay = el.dataset.index * 150
+      el.style.transform = 'translateY(-10%)'
+      const delay = 150
       setTimeout(() => {
         requestAnimationFrame(() => {
           el.style.opacity = 1
@@ -35,24 +24,24 @@ export default {
     afterEnter(el, done) {
       el.style.transform = 'translateY(0)'
       done
-    }
+    },
 
     // leave(el) {
     //   // requestAnimationFrame(())
     // }
-  }
+  },
 }
 </script>
 
 <style>
 .slide-enter-active,
 .slide-leave-active {
-  transition: all 0.8s;
-  transform: translateX(100px);
+  transition: all 0.3s;
+  transform: translateX(0);
 }
 .slide-enter,
 .slide-leave-to {
   opacity: 0;
-  transform: translateY(-100%);
+  transform: translateY(0);
 }
 </style>
